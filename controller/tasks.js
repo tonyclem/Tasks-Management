e3f519fb2ccaf8a3ca536ad82b44f3a58c6ebcda
tonyclem <tonyclem001@gmail.com>
2023-11-04 13:21:13 +0100

    refactor code using middleware async set up

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,13 +1,14 @@
 const Task = require('../models/task')
+const asyncWrapper = require('../middleware/async')
 
-const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({})
-    res.status(200).json({ tasks })
-  } catch (error) {
-    res.status(500).json({ msg: error.message })
-  }
-}
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({})
+  // res.status(200).json({ tasks })
+  // res.status(200).json({ tasks, amount: tasks.length })
+  res
+    .status(200)
+    .json({ status: 'success', data: { tasks, amount: tasks.length } })
+})
 
 const createTask = async (req, res) => {
   try {
