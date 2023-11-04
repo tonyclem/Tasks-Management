c859b3a809321dd96ff1a729af81b4e63fe39b76
tonyclem <tonyclem001@gmail.com>
2023-11-04 10:13:09 +0100

    working on controller

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,13 +1,37 @@
-const getAllTasks = (req, res) => {
-  res.send('all Items updated')
+const Task = require('../models/task')
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks })
+  } catch (error) {
+    res.status(500).json({ msg: error.message })
+  }
 }
 
-const createTask = (req, res) => {
-  res.json(req.body)
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error.message })
+  }
 }
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id })
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findById({ _id: taskID })
+
+    if (!task) {
+      return res.status(404).json({ msg: `Task not found with id: ${taskID}` })
+    }
+
+    console.log(task)
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error.message })
+  }
 }
 
 const updateTask = (req, res) => {
